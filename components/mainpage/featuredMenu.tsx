24f8ed55d4d4ffd178b358ca9a8475675d8c6300
ditoow <ghifariwiraa@gmail.com>
2025-11-27 00:11:07 +0700

    feat: featuredmenu update

--- components/mainpage/featuredMenu.tsx
+++ components/mainpage/featuredMenu.tsx
@@ -37,13 +37,13 @@ export default function FeaturedMenu({
       ) : (
         // Container Scroll Horizontal
         // Class scrollbar-hide trick untuk menyembunyikan scrollbar default browser
-        <div className="px-0.5 ">
+        <div className="px-0.5 pl-4 ">
           <div className="flex gap-3 rounded-2xl overflow-x-auto -mx-4 px-4 md:mx-0 md:px-0 snap-x snap-mandatory [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
             {items.map((item) => (
               <div
                 key={item.id}
                 // Lebar fix (w-28 = 112px) agar muat banyak di layar HP & memicu scroll
-                className="flex-none w-28 md:w-3xs md:h-40 snap-start"
+                className="flex-none  h-35 w-3xs md:w-3xs md:h-40 snap-start"
               >
                 <Card className="overflow-hidden transition-all hover:shadow-md bg-white p-0 h-full border-0 shadow-none group">
                   <CardContent className="p-0 h-full">
